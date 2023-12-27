283e692386ce8bd6d5762ce9d3f9295a1d8369ac
KripaMishra <kripa5661@gmail.com>
2023-12-27 08:38:16 +0530

    batch prediction pipeline added

--- finance_complaint/entity/config_entity.py
+++ finance_complaint/entity/config_entity.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from finance_complaint.constant.prediction_pipeline_config.file_config import ARCHIVE_DIR, INPUT_DIR, FAILED_DIR, PREDICTION_DIR, REGION_NAME
 
 TrainingPipelineConfig= namedtuple("PipelineConfig", ["pipeline_name", "artifact_dir"])
 DataIngestionConfig= namedtuple("DataIngestionConfig", ["from_date",
@@ -33,3 +34,22 @@ ModelEvaluationConfig = namedtuple("ModelEvaluationConfig",
                                    ["model_evaluation_report_file_path", "threshold", "metric_list", "model_dir",
                                     "bucket_name"])
 ModelPusherConfig = namedtuple("ModelPusherConfig", ["model_dir", "bucket_name"])
+
+
+
+class PredictionPipelineConfig:
+
+    def __init__(self, input_dir=INPUT_DIR,
+                 prediction_dir=PREDICTION_DIR,
+                 failed_dir=FAILED_DIR,
+                 archive_dir=ARCHIVE_DIR,
+                 region_name=REGION_NAME
+                 ):
+        self.input_dir = input_dir
+        self.prediction_dir = prediction_dir
+        self.failed_dir = failed_dir
+        self.archive_dir = archive_dir
+        self.region_name = region_name
+
+    def to_dict(self):
+        return self.__dict__
